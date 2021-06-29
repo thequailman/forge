fc3bfaf1a90377d870e1fc57feb5290b11de75cc
Mike McRill <mike@mcrill.net>
2021-06-29 16:17:52 -0500

    wip

--- forge.sh
+++ forge.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-ADDITIONAL=""
 APTCACHER=""
 ARCH="amd64"
 BINFMT=""
 BOOTLOADER=""
-BOOTOPTIONS="elevator=noop root=LABEL=root${LABLE}"
+BOOTOPTIONS="root=LABEL=root${LABEL}"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 IMAGEFORMAT="qcow2"
 IMAGESIZE="10G"
@@ -15,17 +14,13 @@ LABEL=""
 LUKS=""
 OUTPUTPATH="./debian"
 OUTPUTTYPE="filesystem"
-PASSWORD=""
+PACKAGES=""
+PROFILE=""
+ROOTPASSWORD=""
+ROOTKEY="./id_rsa.pub"
 SQUASH=""
 SWAPSIZE=""
 TEMPDIR="$(pwd)/tmp"
-UBOOT=""
-UBOOTIDB=""
-UBOOTITP=""
-UBOOTVERSION="rockchip"
-USERHOME="/home/ansible"
-USERKEY="./id_rsa.pub"
-USERNAME="ansible"
 VERSION="bullseye"
 
 diskpath=""
@@ -37,28 +32,23 @@ function show_usage() {
 
 Arguments:
   -ac [apt-cacher-address]  apt-cacher-ng proxy
-  -ap [packages]            additional packages to include after install
+  -ap [packages]            additional packages to include
   -ar [architecture]        architecture (amd64, arm64) (default: ${ARCH})
-  -bl                       bootloader (bios, uboot, uefi) (default: none/no bootloader or kernel)
   -bo                       boot options
   -d                        enable debugging
-  -if                       image format (qcow2, raw) (default: ${IMAGEFORMAT})
-  -iw [size]                image swap size (default: none/no swap)
   -h                        show help
+  -if                       image format (qcow2, raw) (default: ${IMAGEFORMAT})
+  -is [size]                image size (default: ${IMAGESIZE})
   -la                       append a custom label to partition names
   -lu                       enable LUKS for image
   -o [path]                 output directory (filesystem), target disk (disk) or filename without extension (image) (default: ${OUTPUTPATH})
-  -p [password]             root password (default: none)
+  -p [profile]              use a specific hardware profile (odroidn2, pinebookpro) (default: none)
+  -rk [password]            filename for root SSH public key (default: ${ROOTKEY})
+  -rp [password]            root password (default: none)
   -sq                       have output be squashfs
   -sw [size]                swap size (default: none/no swap)
   -t [path]                 temporary files directory (default: ${TEMPDIR})
-  -ub                       setup u-boot
-  -uh [path]                home dir for initial user (default: ${USERHOME})
-  -ui [path]                u-boot IDB path (idbloader.img) (default: none/no idb)
-  -uk [filename]            filename for SSH public key to add to initial user (default: ${USERKEY})
-  -un [username]            username for initial user (default: ${USERNAME})
-  -uo [path]                u-boot ITB path (u-boot.itb) (default: none/no itp)
-  -uv [version]             u-boot version to install (default: ${UBOOTVERSION})
+  -u                        enable UEFI (default: no bootloader)
   -v [version]              debian version to install (default: buster)"
   exit 0
 }
@@ -78,51 +68,53 @@ function provision() {
 
   if [ "${diskpath}" ]; then
     if [ ! -e "/dev/lvm${LABEL}/root${LABEL}" ]; then
-      if [ "${UBOOT}" ]; then
-        parted "${diskpath}" mklabel gpt
-        dd "if=${UBOOTIDB}" conv=notrunc seek=64 "of=${diskpath}"
-        dd "if=${UBOOTITP}" conv=notrunc seek=16384 "of=${diskpath}"
-        parted "${diskpath}" mkpart primary 32768s 442367s
-        parted "${diskpath}" set 1 esp on
-        sleep 1
-        mkfs.vfat -n "efi${LABEL}" "${diskpath}${partition}1"
-        parted "${diskpath}" mkpart primary 442368s 100%
-      else
-        case "${BOOTLOADER}" in
-          bios)
-            parted "${diskpath}" mklabel msdos
-            parted "${diskpath}" mkpart primary 1MiB 200MiB
-            parted "${diskpath}" set 1 boot on
-	    sleep 1
-            mkfs.ext4 -L "boot${LABEL}" "${diskpath}${partition}1"
-            parted "${diskpath}" mkpart primary 200MiB 100%
-          ;;
-          uefi)
+      case "${PROFILE}" in
+        odroidn2)
+        ;;
+        pinebookpro)
+          BOOTLOADER="yes"
+          parted "${diskpath}" mklabel gpt
+          dd if=/usr/lib/u-boot/pinebook-pro-rk3399/idbloader.img conv=notrunc seek=64 "of=${diskpath}"
+          dd if=/usr/lib/u-boot/pinebook-pro-rk3399/u-boot.itb conv=notrunc seek=16384 "of=${diskpath}"
+          parted "${diskpath}" mkpart primary 32768s 442367s
+          parted "${diskpath}" set 1 esp on
+          sleep 1
+          mkfs.vfat -n "efi${LABEL}" "${diskpath}${partition}1"
+          parted "${diskpath}" mkpart primary 442368s 100%
+        ;;
+        default)
+          if [ "${BOOTLOADER}" ]; then
             parted "${diskpath}" mkpart primary fat32 1MiB 200MiB
             parted "${diskpath}" set 1 esp on
-	    sleep 1
+	          sleep 1
             mkfs.vfat -n "efi${LABEL}" "${diskpath}${partition}1"
             parted "${diskpath}" mkpart primary 200MiB 100%
-          ;;
-        esac
-      fi
+          fi
+        ;;
+      esac
 
       if [ "${LUKS}" ]; then
         cryptsetup --label "luks${LABEL}" -v luksFormat --type luks2 "${diskpath}${partition}2"
         cryptsetup open "/dev/disk/by-label/luks${LABEL}" "luks${LABEL}"
-        vgcreate "lvm${LABEL}" "/dev/mapper/luks${LABEL}"
+        mkfs.btrfs -L "btrfs${LABEL}" "/dev/mapper/luks${LABEL}"
       else
-        vgcreate "lvm${LABEL}" "${diskpath}${partition}2"
+        mkfs.btrfs -L "btrfs${LABEL}" "/dev/mapper/luks${LABEL}"
       fi
+
+      mkdir -p "${TEMPDIR}"
+      mount "/dev/disk/by-label/btrfs${LABEL}" "${TEMPDIR}"
+
+      btrfs sub create "${TEMPDIR}/debian-${VERSION}"
 
       if [ "${SWAPSIZE}" ]; then
-        lvcreate -n "swap${LABEL}" -L "${SWAPSIZE}" "lvm${LABEL}"
-        mkswap -L "swap${LABEL}" "/dev/lvm${LABEL}/swap${LABEL}"
+        btrfs sub create "${TEMPDIR}/@swap"
+        fallocate -L "${SWAPSIZE}" "${TEMPDIR}/@swap/swapfile"
+        chmod 0600 "${TEMPDIR}/@swap/swapfile"
+        chattr +C "${TEMPDIR}/@swap/swapfile"
+        btrfs property set "${TEMPDIR}/@swap/swapfile" compression none
       fi
 
-      lvcreate -n "root${LABEL}" -l "100%FREE" "lvm${LABEL}"
-      mkfs.ext4 -L "root${LABEL}" "/dev/lvm${LABEL}/root${LABEL}"
-      sleep 1
+      umount "/dev/disk/by-label/btrfs${LABEL}"
     fi
   fi
 }
@@ -142,20 +134,16 @@ function mountfs() {
       cryptsetup open "/dev/disk/by-label/luks${LABEL}" "luks${LABEL}"
     fi
 
-    mount "/dev/disk/by-label/root${LABEL}" "${TEMPDIR}" || true
+    mount "/dev/disk/by-label/btrfs${LABEL}" "${TEMPDIR}"
 
     if [ "${BOOTLOADER}" == uefi ]; then
       mkdir -p "${TEMPDIR}/boot/efi"
       mount "/dev/disk/by-label/efi${LABEL}" "${TEMPDIR}/boot/efi" || true
-    elif [ "${BOOTLOADER}" == bios ]; then
-      mount "/dev/disk/by-label/boot${LABEL}" "${TEMPDIR}/boot" || true
     fi
   fi
 }
 
 function install() {
-  PACKAGES=""
-
   if [ "${BOOTLOADER}" ]; then
     PACKAGES="linux-image-${ARCH},${PACKAGES}"
   fi
@@ -183,7 +171,7 @@ function install() {
       deboptions="--foreign"
     fi
 
-    debootstrap "${deboptions}" --arch "${ARCH}" --components=main,contrib,non-free --include "apt-transport-https,ca-certificates,curl,dbus,gnupg2,jq,libpam-systemd,locales,openssh-server,policykit-1,python3-minimal,sudo,usrmerge,unzip,${PACKAGES}" --exclude "cron,ifupdown,iptables,logrotate,nano,rsyslog,${exclude}" "${VERSION}" "${TEMPDIR}" "http://${APTCACHER}deb.debian.org/debian"
+    debootstrap "${deboptions}" --arch "${ARCH}" --components=main,contrib,non-free --include "apt-transport-https,ca-certificates,curl,gnupg2,jq,libpam-systemd,locales,openssh-server,policykit-1,python3-minimal,${PACKAGES}" --exclude "cron,ifupdown,logrotate,nano,rsyslog,${exclude}" "${VERSION}" "${TEMPDIR}" "http://${APTCACHER}deb.debian.org/debian"
 
     if ! uname -a | grep "${ARCH}"; then
       cp "/usr/bin/qemu-${BINFMT}-static" "${TEMPDIR}/usr/bin"
@@ -196,40 +184,19 @@ function configure() {
   # Edit sources
   cat > "${TEMPDIR}/etc/apt/sources.list" << EOF
 deb http://${APTCACHER}deb.debian.org/debian ${VERSION} main contrib non-free
-EOF
-  if [ ${VERSION} = "bullseye" ]; then
-    cat >> "${TEMPDIR}/etc/apt/sources.list" << EOF
 deb http://${APTCACHER}deb.debian.org/debian ${VERSION}-updates main contrib non-free
 deb http://${APTCACHER}security.debian.org/debian-security ${VERSION}-security main contrib non-free
 EOF
-  fi
 
   # Dynamic hostname
   rm -f "${TEMPDIR}/etc/hostname" || true
 
+  # Prevent packages from starting services
   cat > "${TEMPDIR}/usr/sbin/policy-rc.d" << EOF
 exit 101
 EOF
 
-  # Install additional packages
-  if [ "${ADDITIONAL}" ]; then
-    if ! grep -qs "${TEMPDIR}/dev" /proc/mounts; then
-      mount -o bind /dev "${TEMPDIR}/dev"
-    fi
-    if ! grep -qs "${TEMPDIR}/sys" /proc/mounts; then
-      mount -o bind /sys "${TEMPDIR}/sys"
-    fi
-    if ! grep -qs "${TEMPDIR}/proc" /proc/mounts; then
-      mount -o bind /proc "${TEMPDIR}/proc"
-    fi
-    cp /etc/resolve.conf "${TEMPDIR}/etc/resolv.conf"
-    chroot "${TEMPDIR}" /bin/bash -c "apt update && apt install -y ${ADDITIONAL}"
-    rm "${TEMPDIR}/etc/resolv.conf"
-    umount "${TEMPDIR}/dev"
-    umount "${TEMPDIR}/sys"
-    umount "${TEMPDIR}/proc"
-  fi
-
+  # Default systemd networking
   cat > "${TEMPDIR}/etc/systemd/network/default.network" << EOF
 [Match]
 Name=!lo
@@ -254,35 +221,21 @@ EOF
 FallbackDNS=1.1.1.1
 EOF
 
-  # Create user
-  chroot "${TEMPDIR}" useradd -c "${USERNAME}" -d "${USERHOME}" -mu 2000 "${USERNAME}" || true
-  cat > "${TEMPDIR}/etc/sudoers" << EOF
-Defaults env_reset
-Defaults mail_badpass
-Defaults secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-root ALL=(ALL:ALL) ALL
-
-%admin ALL=(ALL) NOPASSWD: ALL
-EOF
-  chroot "${TEMPDIR}" groupadd admin || true
-  chroot "${TEMPDIR}" gpasswd -a "${USERNAME}" admin || true
-  chroot "${TEMPDIR}" mkdir -p "${USERHOME}"/.ssh
-  chroot "${TEMPDIR}" mkdir -p "${USERHOME}"/.ssh
-  chroot "${TEMPDIR}" touch "${USERHOME}"/.ssh/authorized_keys
-  chroot "${TEMPDIR}" chown "${USERNAME}:${USERNAME}" "${USERHOME}/.ssh"
-  cp "${USERKEY}" "${TEMPDIR}${USERHOME}"/.ssh/authorized_keys
+  # Set root SSH key
+  mkdir -p "${TEMPDIR}/root/.ssh"
+  cp "${ROOTKEY}" "${TEMPDIR}/root/.ssh"/.ssh/authorized_keys
 
   # Set root password
-  if [ "${PASSWORD}" ]; then
-    echo "root:${PASSWORD}" | chpasswd -R "${TEMPDIR}"
+  if [ "${ROOTPASSWORD}" ]; then
+    chroot "${TEMPDIR}" 
+    echo "root:${ROOTPASSWORD}" | chpasswd -R "${TEMPDIR}"
   fi
 }
 
 function finalize() {
   chroot "${TEMPDIR}" apt clean
 
-  if [ "${BOOTLOADER}" == uefi ]; then
+  if [ "${BOOTLOADER}" ]; then
     cat > "${TEMPDIR}/etc/fstab" << EOF
 LABEL=efi${LABEL} /boot/efi vfat defaults 0 1
 EOF
@@ -297,7 +250,7 @@ for kernel in /boot/vmlinuz-*; do
   cp -f "/boot/initrd.img-\${version}" /boot/efi/
   cp -f "/boot/vmlinuz-\${version}" /boot/efi/
   cat > /boot/efi/loader/entries/debian10-\${version}.conf << EOF
-title Debian 10 - \${version}
+title Debian ${VERSION} - \${version}
 linux /vmlinuz-\${version}
 initrd /initrd.img-\${version}
 options ${BOOTOPTIONS} rw
@@ -334,21 +287,15 @@ EOF
       mount -o bind /dev "${TEMPDIR}/dev"
       mount -t sysfs /sys "${TEMPDIR}/sys"
       mount -t proc /proc "${TEMPDIR}/proc"
-      chroot "${TEMPDIR}" bootctl --esp-path "${TEMPDIR}/boot/efi" install || true
+      chroot "${TEMPDIR}" bootctl --esp-path /boot/efi install || true
       umount "${TEMPDIR}/dev"
       umount "${TEMPDIR}/sys"
       umount "${TEMPDIR}/proc"
     fi
 
     cat > "${TEMPDIR}/boot/efi/loader/loader.conf" << EOF
-default debian10-*
+default debian${VERSION}-*
 timeout 3
-EOF
-
-    cat > "${TEMPDIR}/boot/efi/loader/entries/debian.conf" << EOF
-title Debian 10 -
-linux /vmlinuz
-initrd /initrd.img
 EOF
 
     if [ "${LUKS}" ]; then
@@ -369,32 +316,14 @@ EOF
 
   if [ "${diskpath}" ]; then
     cat >> "${TEMPDIR}/etc/fstab" << EOF
-LABEL=root${LABEL} / ext4 defaults,noatime 0 1
+LABEL=btrfs${LABEL} / btrfs defaults 0 0
 EOF
 
     if [ "${SWAPSIZE}" ]; then
       cat >> "${TEMPDIR}/etc/fstab" << EOF
-LABEL=swap${LABEL} none swap defaults 0 0
+LABEL=btrfs${LABEL} /swap btrfs defaults,subvol=@swap 0 0
+/swap/swapfile none swap defaults 0 0
 EOF
-    fi
-
-    if [ "${BOOTLOADER}" == bios ]; then
-      cat >> "${TEMPDIR}/etc/fstab" << EOF
-LABEL=boot${LABEL} /boot ext4 defaults,noatime 0 1
-EOF
-      mount -o bind /dev "${TEMPDIR}/dev"
-      mount -t proc /proc "${TEMPDIR}/proc"
-      mount -t sysfs /sys "${TEMPDIR}/sys"
-
-      sed -i 's/#GRUB_TERMINAL/GRUB_TERMINAL/g' "${TEMPDIR}/etc/default/grub"
-      sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=""/g' "${TEMPDIR}/etc/default/grub"
-      chroot "${TEMPDIR}" grub-install "${diskpath}"
-      chroot "${TEMPDIR}" grub-mkconfig -o /boot/grub/grub.cfg
-
-      sleep 1
-      umount "${TEMPDIR}/dev"
-      umount "${TEMPDIR}/proc"
-      umount "${TEMPDIR}/sys"
     fi
   fi
 
@@ -440,7 +369,6 @@ EOF
 
   if [ "${diskpath}" ]; then
     umount -R "${TEMPDIR}" || true
-    vgchange -an "/dev/lvm${LABEL}" || true
     qemu-nbd -d /dev/nbd0 || true
     rmdir "${TEMPDIR}"
 
@@ -470,7 +398,7 @@ while [ $# -gt 0 ]; do
       shift 2
     ;;
     -ap)
-      ADDITIONAL="${2}"
+      PACKAGES="${2}"
       shift 2
     ;;
     -ar)
@@ -481,17 +409,6 @@ while [ $# -gt 0 ]; do
         arm64)
           ARCH=arm64
 	  BINFMT=aarch64
-        ;;
-      esac
-      shift 2
-    ;;
-    -bl)
-      case "${2}" in
-        bios)
-          BOOTLOADER=bios
-        ;;
-        uefi)
-          BOOTLOADER=uefi
         ;;
       esac
       shift 2
@@ -535,11 +452,19 @@ while [ $# -gt 0 ]; do
       shift 2
     ;;
     -p)
-      PASSWORD="${2}"
+      PROFILE="${2}"
+      shift 2
+    ;;
+    -rk)
+      ROOTKEY="${2}"
+      shift 2
+    ;;
+    -rp)
+      ROOTPASSWORD="${2}"
       shift 2
     ;;
     -sq)
-      BOOTLOADER=squash
+      BOOTLOADER=yes
       SQUASH=yes
       shift 1
     ;;
@@ -551,34 +476,9 @@ while [ $# -gt 0 ]; do
       TEMPDIR="${2}"
       shift 2
     ;;
-    -ub)
-      BOOTLOADER=uefi
-      UBOOT=yes
+    -u)
+      BOOTLOADER=yes
       shift 1
-    ;;
-    -uh)
-      USERHOME="${2}"
-      shift 2
-    ;;
-    -ui)
-      UBOOTIDB="${2}"
-      shift 2
-    ;;
-    -uk)
-      USERKEY="${2}"
-      shift 2
-    ;;
-    -un)
-      USERNAME="${2}"
-      shift 2
-    ;;
-    -uo)
-      UBOOTITP="${2}"
-      shift 2
-    ;;
-    -uv)
-      UBOOTVERSION="${2}"
-      shift 2
     ;;
     -v)
       VERSION="${2}"
@@ -633,8 +533,17 @@ if [ "${SQUASH}" ] && ! [ -x "$(command -v mksquashfs)" ]; then
   INSTALLDEPS+=" squashfs-tools"
 fi
 
+case "${PROFILE}" in
+  odroidn2)
+    INSTALLDEPS+="u-boot-amlogic"
+  ;;
+  pinebookpro)
+    INSTALLDEPS+="u-boot-rockchip"
+  ;;
+esac
+
 if [ "${INSTALLDEPS}" ]; then
-  apt update && apt install -y ${INSTALLDEPS}
+  apt update && apt install -y "${INSTALLDEPS}"
 fi
 
 provision
